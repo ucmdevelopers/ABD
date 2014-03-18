9031e1427d39d5562639f588f09c130c27868c95
rnov <rsmarinov@gmail.com>
2014-03-18 12:35:13 +0100

    Abstract mapper acabado, todas las funciones génericas que faltaba por añadir(update, delete,insert) aparte de un cambio en el findId que no lo hacía bien. Aprte también he añadido y implementado las abstractas que se utilizan dentro de las génericas. y que son propias de los mappers que las heredan. He añadido un par de métodos privados para que sea más claro el código de las génericas ya son muy largas, pero se puede prescindir de ellas. También he adaptado el usuario. Todo lo mio está comentado y separado de lo que hiciste tú para que te sea más claro seguirlo, también he comentado dentro de tu código pero no tus métodos. Las parser que he implementado están probado en mysql y funcionan bien lo único que no he conseguido es establecer una conexión con elclipse - mysql tal y como lo has puesto tú con Datasource, si lo he conseguido con driver, eso me queda por averiguar

--- practica1/src/database/UsuarioMapper.java
+++ practica1/src/database/UsuarioMapper.java
@@ -1,6 +1,7 @@
 package database;
 
-import java.math.BigInteger;
+//import java.math.BigInteger;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -9,14 +10,26 @@ import javax.sql.DataSource;
 import model.UserKeys;
 import model.Usuario;
 
+
+
+
+
+//-----para pruebas main
+import java.sql.*;
+//---
 public class UsuarioMapper extends AbstractMapper<Usuario,UserKeys>{
 
-	
+	private Usuario usr; 
 
 	public UsuarioMapper(DataSource dataSource) {
 		super(dataSource);
 		// TODO Auto-generated constructor stub
 	}
+	public UsuarioMapper(DataSource dataSource, Usuario _usr) {
+		super(dataSource);
+		this.usr= _usr;
+	}
+
 
 	@Override
 	protected String getTableName() {
@@ -24,56 +37,85 @@ public class UsuarioMapper extends AbstractMapper<Usuario,UserKeys>{
 		return model.Constants.USER_TABLE ;
 	}
 
-
-	
-
-
 	@SuppressWarnings("null")
 	@Override
 	protected Usuario buildObject(ResultSet rs) throws SQLException {
 		// TODO Auto-generated method stub
 		 Usuario result = null;
-		 result.setId_user(new BigInteger(Integer.valueOf(rs.getInt("id_user")).toString()));
-		 result.setNombre(rs.getString("nombre"));
-		 result.setApell0(rs.getString("apellido0"));
-		 result.setApell0(rs.getString("apellido1"));
-		 result.setSexo( rs.getString("sexo").charAt(0));
-		 result.setNac(rs.getDate("fecha_nacimiento"));
-		 result.setLog(rs.getString("log"));
-		 result.setPass(rs.getString("pass"));
+		 result.setId_user(rs.getString("id_user"));
+		 result.setPasswd(rs.getString("passwd"));
 		return result;
 	}
-
-	
-	
 
 	@Override
 	protected Object[] serializeObject(Usuario object) {
 		// TODO Auto-generated method stub
 		
-		return new Object[] {object.getId_user(),object.getNombre(),object.getApell0(),object.getSexo(),object.getNac(),object.getLog(),object.getPass()};
-	}
-
-	
-
-	@Override
-	protected Object[] serializeKey(UserKeys key) {
-		// TODO Auto-generated method stub
-		
-		return new Object[]{key.getKey()};
-	}
-
-	@Override
-	protected UserKeys getKey(Usuario obj) {
-		// TODO Auto-generated method stub
-		return new UserKeys(obj.getId_user());
+		return new Object[] {object.getId_user(),object.getPasswd()};
 	}
 
 	@Override
 	protected String[] getColumnNames() {
 		// TODO Auto-generated method stub
-		 	return new String[]{"id_user", "nombre", "apellido0", "apellido1", "sexo", "fecha_nacimiento", "log", "pass"};
+		 	return new String[]{"nick", "passwd", "fecha_nac", "img"};
 	}
+
+	
+	@Override
+	public String pharseUpdate(Usuario obj) {
+		String str =  "passwd"+"="+obj.getPasswd()+" where "+" nick"+" = "+obj.getId_user();
+		return str;
+	}
+	@Override
+	public String pharseInsert(Usuario obj) {
+		String str = "(nick , passwd)" + " value " + " ( " +obj.getId_user() + " , " + obj.getPasswd()  + " )";
+		return str;
+	}
+	@Override
+	public String pharseDelete(UserKeys id) {
+		String str ="nick "+ "="+id ;
+		return str;
+	}
+	
+	
+	
+	//No se necesitan estos metodos en está clase por eso no se implementan
+	@Override
+	protected UserKeys getKey(Usuario obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected Object[] serializeKey(UserKeys key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	 public static void main(String args[])
+		        throws Exception
+		    {
+		//The DataSource interface as an alternative to the DriverManager for establishing a connection with a data source
+		 		//Accessing driver
+		 		Class.forName("com.mysql.jdbc.Driver");
+		 		//creating a variable for the connection
+		 		//Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/practica11","root","");
+		 		//Connection con = DataSource.getConnection("jdbc:mysql://localhost:3306/practica11","root","");
+		 		
+		 		//DataSource data= 
+		 		//UsuarioMapper s= new UsuarioMapper(); 
+		 		/*PreparedStatement statement = con.prepareStatement("select * from usuarios");
+		 		ResultSet result = statement.executeQuery();
+		 		while(result.next())
+		 		{
+		 			System.out.println(result.getString(1)+""+result.getString(2));
+		 		}*/
+		    
+		    
+		    
+		    
+		    }
+	
 
 	
 
